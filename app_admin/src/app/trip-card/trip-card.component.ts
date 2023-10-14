5416eb1b3dcd97fc7ccea0a000d8d35cfe91fd48
Kyle-Conley <kwconley93@gmail.com>
2023-10-14 15:07:21 -0400

    Implement Frontend Security in MEAN stack

--- app_admin/src/app/trip-card/trip-card.component.ts
+++ app_admin/src/app/trip-card/trip-card.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { Router } from "@angular/router";
 import { Trip } from '../models/trip';
+import { AuthenticationService } from '../services/authentication';
 
 @Component({
   selector: 'app-trip-card',
@@ -9,20 +10,24 @@ import { Trip } from '../models/trip';
 })
 export class TripCardComponent implements OnInit {
 
-  @Input('trip') trip: any;
+  @Input('trip') trip: Trip;
 
   constructor(
-    private router: Router
+    private router: Router,
+    private authenticationService: AuthenticationService
   ) { }
 
-  ngOnInit() {
+  ngOnInit(): void {
   }
 
-  private editTrip(trip: Trip): void {
-    console.log('Inside TripListingComponent#editTrip');
+  public isLoggedIn() : boolean {
+    return this.authenticationService.isLoggedIn();
+  }
+  
+  public editTrip(trip: Trip): void {
     localStorage.removeItem("tripCode");
     localStorage.setItem("tripCode", trip.code);
-    this.router.navigate(["edit-trip"]);
+    this.router.navigate(['edit-trip']);
   }
 
 }
--- app_admin/src/app/trip-listing/trip-listing.component.ts
+++ app_admin/src/app/trip-listing/trip-listing.component.ts
@@ -1,8 +1,9 @@
 import { Component, OnInit } from '@angular/core';
-import { Router } from "@angular/router";
-//import { trips } from '../data/trips';
+import { Router } from '@angular/router';
+// import { trips } from '../data/trips';
 import { TripDataService } from '../services/trip-data.service';
 import { Trip } from '../models/trip';
+import { AuthenticationService } from '../services/authentication';
 
 @Component({
   selector: 'app-trip-listing',
@@ -10,36 +11,40 @@ import { Trip } from '../models/trip';
   styleUrls: ['./trip-listing.component.css'],
   providers: [TripDataService]
 })
-
 export class TripListingComponent implements OnInit {
 
   //trips: Array<any> = trips;
   trips: Trip[];
 
   message: string;
-  
+
   constructor(
     private tripDataService: TripDataService,
-    private router: Router
+    private router: Router,
+    private authenticationService: AuthenticationService
+
     ) { }
 
-  private addTrip(): void {
-    console.log('Inside TripListingComponent#addTrip');
-    this.router.navigate(['add-trip']);
-  }  
+    private addTrip(): void {
+      this.router.navigate(['add-trip']);
+    }
 
   private getTrips(): void {
     console.log('Inside TripListingComponent#getTrips');
     this.message = 'Searching for trips';
     this.tripDataService
       .getTrips()
-      .then(foundTrips => {
-        this.message = foundTrips.length > 0 ? '' : 'No trips found';
-        this.trips = foundTrips;
-      });
-   }
+        .then(foundTrips => {
+          this.message = foundTrips.length > 0 ? '' : 'No trips found';
+          this.trips = foundTrips;
+        });
+  }
 
   ngOnInit(): void {
     this.getTrips();
+  }
+
+  public isLoggedIn() : boolean {
+    return this.authenticationService.isLoggedIn();
   }
 }
